6cc038bb0d64c9150f1145cc810b0930304e5fef
Vu Cong Ngon <vucongngon219@gmail.com>
2023-09-03 23:58:22 +0700

    second version

---
+++ control/script.js
@@ -0,0 +1,59 @@
+const accessKey = "4tJS56k2OGrDFZmh60OeiHsEnZhydf34kVycK_Og0yA";
+
+const formEl = document.querySelector("form");
+const inputEl = document.getElementById("search-input");
+const searchResults = document.querySelector(".search-results");
+const showMore = document.getElementById("show-more-option");
+
+let inputData = "";
+let page = 1;
+
+
+async function searchImages() {
+    inputData = inputEl.value;
+    const url = `https://api.unsplash.com/search/photos?page=${page}&query=${inputData}&client_id=${accessKey}`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const results = data.results;
+
+    if(page === 1) {
+        searchResults.innerHTML = "";
+    }
+
+    results.map((result) => {
+        const imagewrapper = document.createElement("div");
+        imagewrapper.classList.add("search-result");
+        const image = document.createElement("img");
+        image.src = result.urls.small;
+        image.alt = result.alt_description;
+        const imageLink = document.createElement("a");
+        imageLink.href = result.links.html;
+        imageLink.target = "_blank";
+        imageLink.textContent = result.alt_description;
+
+        imagewrapper.appendChild(image);
+        imagewrapper.appendChild(imageLink);
+        searchResults.appendChild(imagewrapper);
+    });
+
+    page++;
+    
+    if(page > 1) {
+        showMore.style.display = "block";
+
+    }
+
+}
+
+formEl.addEventListener("submit", (event) => {
+    console.log("click on search ");
+    event.preventDefault();
+    page = 1;
+    searchImages();
+});
+
+showMore.addEventListener("click", () => {
+    searchImages();
+});
